691c8b82e954afad9a4b54af9330696225af37bf
Philip Welin-Berger <philip.welin.berger@gmail.com>
2022-03-30 20:25:13 +0200

    fixed minus sign in guard clause

--- R2.cpp
+++ R2.cpp
@@ -3,7 +3,7 @@
 int main(){
   int R1, S;
   std::cin >> R1 >> S;
-  if (R1 < -1000 || R1 > 1000 || S < 1000 || S > 1000) throw std::invalid_argument("arguments must be between -1000 & 1000!");
+  if (R1 < -1000 || R1 > 1000 || S < -1000 || S > 1000) throw std::invalid_argument("arguments must be between -1000 & 1000!");
   int R2 = 2*S - R1;
   std::cout << R2 << std::endl;
   return 0; 
