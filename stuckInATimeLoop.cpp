5197c9a83b655a14cc3e7ed87edce7dba0b5b4ea
Philip Welin-Berger <philip.welin.berger@gmail.com>
2022-03-31 22:33:47 +0200

    stuck in a time loop first attempt

---
+++ stuckInATimeLoop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  int N;
+  std::cin >> N;
+  for (int i = 1; i <= N; i++) {
+    std::cout << i << " " << "Abracadabra" << std::endl;
+  }
+  return 0; 
+}
