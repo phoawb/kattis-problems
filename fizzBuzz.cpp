4a96abb4da71ae4dd5b10a7353bb0b8a1779204c
Philip Welin-Berger <philip.welin.berger@gmail.com>
2022-05-09 22:10:43 +0200

    even more correct compact fizzbuzz

--- fizzBuzz.cpp
+++ fizzBuzz.cpp
@@ -3,7 +3,7 @@
 
 int main() {
   int x, y, n, i{0};
-  std::cin >> x, y, n;
+  std::cin >> x >> y >> n;
   for (auto &o = std::cout; ++i < (n + 1); o << "\n") {
     i % x ? o : o << "Fizz";
     i % y ? i % x ? o << i : o : o << "Buzz";
