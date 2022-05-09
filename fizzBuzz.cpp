2f2b2aa9b3a99689f60ce71ccb16d984444abb61
Philip Welin-Berger <philip.welin.berger@gmail.com>
2022-05-09 22:07:10 +0200

    compacted fizzbuzz

--- fizzBuzz.cpp
+++ fizzBuzz.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 #include <string>
 
-// this can be further improved by storing the keys 
-// in a hashmap and iterating over that
-void fizzBuzz(){
-  for (int i = 1; i < 101; i++) {
-    std::string output{""};
-    if (i % 3 == 0) {output += "fizz";}
-    if (i % 5 == 0) {output += "buzz";}
-    if (output == "") {output = std::to_string(i);}
-    std::cout << output << std::endl;
-  }
-}
-
 int main() {
-  fizzBuzz();
+  int x, y, n, i{1};
+  std::cin >> x, y, n;
+  for (auto &o = std::cout; ++i < (n + 1); o << "\n") {
+    i % x ? o : o << "Fizz";
+    i % y ? i % x ? o << i : o : o << "Buzz";
+  }
   return 0;
 }
