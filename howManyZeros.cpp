12262c34d99f2d3f7d127892a00b9d43f2816a84
Philip Welin-Berger <philip.welin.berger@gmail.com>
2022-10-06 01:35:09 +0900

    started on how many zeroes problem

---
+++ howManyZeros.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+ 
+
+long findZerosFromZero(long N) {}
+
+int main() {
+  long m, n;
+  while (std::cin >> m >> n && m > 0) {
+    std::cout << m << n << std::endl;
+  }
+  return 0;
+}
--- stopwatch.cpp
+++ stopwatch.cpp
@@ -6,11 +6,9 @@ void toggleWatch(bool & active) {
   else active = false;
 }
 
-int main() {
-  int N, t{0}, mem{0};
-  std::cin >> N;
-  std::string output;
-  bool active;
+int getTime(int N) {
+  int mem{0}, t{0};
+  bool active{false};
   for (int i = 0; i < N; i++) {
     int time;
     std::cin >> time;
@@ -18,7 +16,14 @@ int main() {
     mem = time;
     toggleWatch(active);
   }
-  output = active ? "still running" : std::to_string(t);
-  std::cout << output << std::endl;
+  return t;
+}
+
+int main() {
+  int N;
+  std::cin >> N;
+  std::string output;
+  if(N % 2 != 0) std::cout << "still running" << std::endl;
+  else std::cout << getTime(N) << std::endl;
   return 0;
 }
